ddb3863b9efb005649df5bbfa499b803b9b0e40f
Yashasvi Mishra <mishrayashasvi23@gmail.com>
2024-11-13 02:42:20 +0530

    this is a commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import json
+
 import pickle
 
 from flask import Flask,request,app,jsonify,url_for,render_template
@@ -9,11 +9,12 @@ app=Flask(__name__)
 ## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
@@ -23,5 +24,12 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
-if __name__ == "__main__":
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data.values()).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The predicted house prediction is {}".format(output))
+if __name__=='__main__':
     app.run(debug=True)
